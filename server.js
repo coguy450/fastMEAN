e0889b258895b68f88f276fb756731206a1df815
Eric Hartmann <coguy450@gmail.com>
2016-11-17 11:50:36 -0700

    added favicons

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ swig.setDefaults({ cache: false });
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use('/favicon.ico', express.static('public/images/favicon.ico'));
 
 app.get('/',controller.index);
 
-var server = app.listen('3000', function() {
+var server = app.listen('3005', function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('listening at port:'+ port);
