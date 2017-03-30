b32ddac4210f5533c74a0b8feb9c98cb818397cc
Eric Hartmann <coguy450@gmail.com>
2017-03-30 13:29:12 -0600

    removed facebook stuff

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var controller = require('./app/controllers/server.controller.js');
 var dbConn;
 
 function conMongo(callback) {
-    mongodb.connect('mongodb://localhost/facebookHack', {db: { autoReconnect: true, connectTimeoutMS: 30000 }}, (err, db) => {
+    mongodb.connect('mongodb://localhost/youdb', (err, db) => {
         if (err) {
             console.log(err);
         } else {
@@ -36,69 +36,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/favicon.ico', express.static('public/images/favicon.ico'));
-app.get('/manager', controller.index);
-app.post('/', function(req, res) {
-    var data = req.body;
-    if (data.object === 'page') {
-        data.entry.forEach(function (entry) {
-            var pageID = entry.id;
-            var timeOfEvent = entry.time;
-            console.log(entry);
-            entry.messaging.forEach(function (event) {
-                if (event.message) {
-                    console.log('event', event);
-                    conMongo((db) => {
-                        const messages = db.collection('fbMessages');
-                        messages.insert(event);
-                    })
-                } else {
-                    console.log("Webhook received unknown event: ", event);
-                }
-            });
-        });
 
-        res.sendStatus(200);
-
-    }
-});
-
-
-app.post('/webhook', (req, res) => {
-    console.log(req);
-    res.sendStatus(200);
-})
-
-const sendFacebookMessage = () => {
-    request
-        .post('https://graph.facebook.com/v2.6/me/messages?access_token=TOKEN')
-
-        .set('Accept', 'application/json')
-        .set('Content-Type', 'application/json')
-        .send({ "recipient": {"id":"1089616971154388"},"message" :{"text":"hello, world!"}})
-        .end(function(err, res){
-            if (err || !res.ok) {
-                console.log(err);
-            } else {
-                console.log('yay got ' + JSON.stringify(res.body));
-            }
-        })
-}
-app.get('/sendMessage', sendFacebookMessage);
-
-
-app.get('/messages', (req, res) => {
-    conMongo((db) => {
-        const messages = db.collection('fbMessages');
-        messages.find({}).toArray((error, results) => {
-            res.status(200).send(results);
-        })
-    })
-})
-
-
-
-
-
+app.get('/', controller.index);
 
 var server = app.listen('3005', function() {
     var host = server.address().address;
@@ -125,6 +64,3 @@ if (app.get('env') === 'development') {
 
 
 module.exports = app;
-
-
-
